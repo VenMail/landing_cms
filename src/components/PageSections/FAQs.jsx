1780ff16675ccb7dca1c40a2cae8a1472b1cadb5
Lolu Adelore <adeloreisaac@gmail.com>
2025-04-19 13:05:51 +0100

    Update FAQs.jsx

--- src/components/PageSections/FAQs.jsx
+++ src/components/PageSections/FAQs.jsx
@@ -61,7 +61,7 @@ function FAQs() {
                 </span>
               </button>
               {openIndex === index && (
-                <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
+                <p className="mt-2 text-sm text-gray-600">
                   {faq.answer}
                 </p>
               )}
