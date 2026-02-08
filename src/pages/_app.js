6029f99d40df815344640a00e5d8af90f875a74f
Chibueze Opata <copata@venmail.io>
2026-02-08 08:09:34 +0100

    Add GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,17 +1,28 @@
 import "@/styles/globals.css";
 import MainLayout from "@/components/layout/MainLayout";
 import { SpeedInsights } from "@vercel/speed-insights/next"
-import { Analytics } from "@vercel/analytics/react"
+import Script from "next/script";
 import { CurrencyProvider } from "@/contexts/CurrencyContext";
 
 export default function App({ Component, pageProps }) {
   return (
     <CurrencyProvider>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-18DJTP5FYM"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-18DJTP5FYM');
+        `}
+      </Script>
       <SpeedInsights />
       <MainLayout>
         <Component {...pageProps} />
       </MainLayout>
-      <Analytics />
     </CurrencyProvider>
   );
 }
